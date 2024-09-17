b7543967ccf21c74a3ad97484336a47df22ae06a
unknown <paulberken@yahoo.de>
2024-09-17 21:00:28 +0200

    new

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function loadShopcart(){
-    
+let i = [];
+
+
+
+
+
+function loadShopcart(id) {
+    document.getElementById(id).classList.toggle('d-none');
+}
+
+function addMargherita() {
+    document.getElementById('marlist').innerHTML = (`1x Margherita = 9€`)
+}
+
+function removeMargherita() {
+    document.getElementById('marlist').innerHTML = (``)
+}
+
+function addSalami() {
+    document.getElementById('sallist').innerHTML = (`1x Salami = 9€`)
+}
+
+function removeSalami() {
+    document.getElementById('sallist').innerHTML = (``)
 }
